727c45a98420908d1242919bcc425fc3d5af3f75
Jóhann Gíslason <johanng13@i-f9b44f70.greenqloud.internal>
2014-10-31 12:22:38 +0000

    Added PrintBoard

--- src/main/java/is/ru/tictactoe/tictactoe.java
+++ src/main/java/is/ru/tictactoe/tictactoe.java
@@ -2,8 +2,28 @@ package is.ru.tictactoe;
 
 public class tictactoe {
 
+	public static int ROWS = 3;
+	public static int COLS = 3;
+	public static int[][] Board = new int[ROWS][COLS];
+
 	public static void main(String args []){
-		System.out.println("Hello World!");
+		PrintBoard();
+	}
+
+	public static void PrintBoard()
+	{
+		System.out.print("------------- \n");
+		for (int rows = 0; rows < ROWS; rows++)
+		{
+			System.out.print("| ");
+			for (int cols = 0; cols < COLS; cols++)
+			{
+				System.out.print(Board[rows][cols]);
+				System.out.print(" | ");
+			}
+			System.out.print("\n");
+		}
+		System.out.print("------------- \n");
 	}
 
 	public static int number(int a) {
