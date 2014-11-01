94037a48c821e3ea54431f62b31297a737c5dd9f
Gunnhildur Finnsdóttir <gunnhildurf13@i-f9b44f70.greenqloud.internal>
2014-11-01 12:33:52 +0000

    clean up of tictactoe.java

--- src/main/java/is/ru/tictactoe/tictactoe.java
+++ src/main/java/is/ru/tictactoe/tictactoe.java
@@ -1,40 +1,11 @@
 package is.ru.tictactoe;
 
 public class tictactoe {
-
-	public static int ROWS = 3;
-	public static int COLS = 3;
-	//public static int[][] Board = new int[ROWS][COLS];
 	
 	public static void main(String args []){
                 Board b = new Board();
                 Board.PrintBoard(b.fields);
         }
-	/*public static void PrintBoard()
-	{
-		System.out.print("------------- \n");
-		for (int rows = 0; rows < ROWS; rows++)
-		{
-			System.out.print("| ");
-			for (int cols = 0; cols < COLS; cols++)
-			{
-				System.out.print(Board[rows][cols]);
-				System.out.print(" | ");
-			}
-			System.out.print("\n");
-		}
-		System.out.print("------------- \n");
-	}*/
-
-	public static int number(int a) {
-		
-		return a;
-	}
-
-	public static String layout () {
-		
-		return "a";
-	}
 }
 
 
--- src/test/java/is/ru/tictactoe/tictactoe_test.java
+++ src/test/java/is/ru/tictactoe/tictactoe_test.java
@@ -6,20 +6,13 @@ import org.junit.Test;
 public class tictactoe_test {
 
 	public static void main(String args []){
-		int x = 1;
 		org.junit.runner.JUnitCore.main("is.ru.tictactoe.tictactoe_test");
 	}
-
+	/*
 	@Test	
 	public  void numberTest(){
 
-		assertEquals(1, tictactoe.number(1));
-	}
-
-	@Test	
-	public  void layoutTest(){
-
-		assertEquals("a", tictactoe.layout());
-	}
+		assertEquals(1, tictactoe.number(1);
+	}*/
 
 }
