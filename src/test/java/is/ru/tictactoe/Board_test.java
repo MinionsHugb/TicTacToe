fa05bf613f8a8479c3b6cb5a2f2ad96ef2c6a583
Jóhann Gíslason <johanng13@i-f9b44f70.greenqloud.internal>
2014-11-01 11:12:44 +0000

    Added test for hasWon column 1 in Board test class

--- src/test/java/is/ru/tictactoe/Board_test.java
+++ src/test/java/is/ru/tictactoe/Board_test.java
@@ -68,6 +68,15 @@ public class Board_test {
                 }
 
 		@Test
+                public void hasWonWin4(){
+                	int[] temp = new int[9];
+                	temp[0] = 2;
+                	temp[3] = 2;
+                	temp[6] = 2;
+                	assertEquals(true, Board.hasWon(temp));
+                }
+
+		@Test
                 public void hasWonLose1(){
                 	int[] temp = new int[9];
                 	temp[0] = 1;
@@ -91,6 +100,15 @@ public class Board_test {
                 	temp[6] = 1;
                 	temp[7] = 1;
                 	temp[8] = 2;
+                	assertEquals(false, Board.hasWon(temp));
+                }
+
+		@Test
+                public void hasWonLose4(){
+                	int[] temp = new int[9];
+                	temp[0] = 2;
+                	temp[3] = 2;
+                	temp[6] = 1;
                 	assertEquals(false, Board.hasWon(temp));
                 }
 }
