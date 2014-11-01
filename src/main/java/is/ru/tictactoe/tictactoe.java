bf31939a395127cb6ce5c23023fb855a66a11fab
Jóhann Gíslason <johanng13@i-f9b44f70.greenqloud.internal>
2014-11-01 15:42:53 +0000

    Added logic to main

--- src/main/java/is/ru/tictactoe/tictactoe.java
+++ src/main/java/is/ru/tictactoe/tictactoe.java
@@ -1,11 +1,43 @@
 package is.ru.tictactoe;
 
+import java.util.Scanner;
+
 public class tictactoe {
-	
-	public static void main(String args []){
+
+        public static void main(String args []){
                 Board b = new Board();
-                Board.PrintBoard(b.fields);
+                Move m = new Move();
+                boolean player1 = true;
+                while(!b.isFull(b.fields) || !b.hasWon(b.fields)){
+                        b.PrintBoard(b.fields);
+                        if(player1){
+                                System.out.println("It's your turn Player 1!");
+                                m.insertMove(1, b.fields);
+                                System.out.println("Got pass insertMove");
+                        }
+                        else{
+                                System.out.println("It's your turn Player 2!");
+                                m.insertMove(2, b.fields);
+                        }
+                        if(player1){
+                                player1 = false;
+                        }
+                        else{
+                                player1 = true;
+                        }
+
+                }
+                if(b.isTie(b.fields)){
+                        System.out.println("DRAW!");
+                }
+                else{
+                        if(!player1){
+                                System.out.println("VICTORY for Player 1");
+                        }
+                        else{
+                                System.out.println("VICTORY for Player 2");
+                        }
+                }
         }
 }
-
 
