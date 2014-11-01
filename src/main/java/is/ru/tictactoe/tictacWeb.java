d4d9894a1823a437dfc0ba9dc1513248770bf5f9
Þórhildur Guðný Sigþórsdóttir <thorhildur13@i-f9b44f70.greenqloud.internal>
2014-11-01 17:54:32 +0000

    Heroku

--- src/main/java/is/ru/tictactoe/tictacWeb.java
+++ src/main/java/is/ru/tictactoe/tictacWeb.java
@@ -16,15 +16,49 @@ public class tictacWeb implements SparkApplication {
     }
 
     public void init(){
-        final Move tic = new Move();
+        final Move m = new Move();
 	final Board b = new Board();
 
         post(new Route("/move"){
             @Override
             public Object handle(Request request, Response response){
-                int [] letter = tic.insertMove(1,b.fields,4);
-                return letter[4];
+                Board b = new Board();
+                Move m = new Move();
+		int button;
+                boolean player1 = true;
+                while(!b.isFull(b.fields) || !b.hasWon(b.fields)){
+                        b.PrintBoard(b.fields);
+                        if(player1){
+                                //return "It's your turn Player 1!";
+				button = 2;
+                                m.insertMove(1, b.fields, button);
+                                //return "Got pass insertMove";
+                        }
+                        else{
+                                //return "It's your turn Player 2!";
+				button = 3;
+                                m.insertMove(2, b.fields, button);
+                        }
+                        if(player1){
+                                player1 = false;
+                        }
+                        else{
+                                player1 = true;
+                        }
+
+                }
+                if(b.isTie(b.fields)){
+                        return "DRAW!";
+                }
+                else{
+                        if(!player1){
+                                return "VICTORY for Player 1";
+                        }
+                        else{
+                                return "VICTORY for Player 2";
+                        }
             }
-        });
-    }
+	}
+         });
+}
 }
