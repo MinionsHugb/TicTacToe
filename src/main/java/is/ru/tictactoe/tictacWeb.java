4573d2b2e3968441165d0c6224f3f659c1a5681f
Andri Páll Pálsson <andripp@i-f9b44f70.greenqloud.internal>
2014-11-02 13:08:08 +0000

    heroku add

--- src/main/java/is/ru/tictactoe/tictacWeb.java
+++ src/main/java/is/ru/tictactoe/tictacWeb.java
@@ -21,47 +21,46 @@ public class tictacWeb implements SparkApplication {
             Board b = new Board();
             //Move m = new Move();
             boolean player1 = true;
-            char xoro = "";
+            //char xoro = '';
             //while(!b.isFull(b.fields) || !b.hasWon(b.fields)){
                 //b.PrintBoard(b.fields);
-
                 post(new Route("/move"){
-                @Override
-                public Object handle(Request request, Response response){
-                    Integer button = Integer.valueOf(request.queryParams("move"));
+                    @Override
+                    public Object handle(Request request, Response response){
+                        Integer button = Integer.valueOf(request.queryParams("move"));
 
-                                return "X";
-                    if(player1){
-                            //return "It's your turn Player 1!";
-                            b.fields = Move.insertMove(1, b.fields, button);
-                            xoro = 'X';
-                            //return "Got pass insertMove";
-                    }
-                    else{
-                                    //return "It's your turn Player 2!";
-                    //button = 3;
-                                    b.fields = m.insertMove(2, b.fields, button);
-                                    xoro = 'O';
-                    }
-                    if(player1){
-                            player1 = false;
-                    }
-                    else{
-                            player1 = true;
-                    }
+                        return "X";
+                      /*  if(player1){
+                                //return "It's your turn Player 1!";
+                                b.fields = Move.insertMove(1, b.fields, button);
+                                xoro = 'X';
+                                //return "Got pass insertMove";
+                        }
+                        else{
+                                        //return "It's your turn Player 2!";
+                        //button = 3;
+                                        b.fields = m.insertMove(2, b.fields, button);
+                                        xoro = 'O';
+                        }
+                        if(player1){
+                                player1 = false;
+                        }
+                        else{
+                                player1 = true;
+                        }
 
-                    }
-                    if(b.isTie(b.fields)){
-                            return "DRAW!";
-                    }
-                    else{
-                            if(!player1){
-                                    return "VICTORY for Player 1";
-                            }
-                            else{
-                                    return "VICTORY for Player 2";
-                            }
-                    }
+                        }
+                        if(b.isTie(b.fields)){
+                                return "DRAW!";
+                        }
+                        else{
+                                if(!player1){
+                                        return "VICTORY for Player 1";
+                                }
+                                else{
+                                        return "VICTORY for Player 2";
+                                }
+                        } */
                 }
         });
     }
